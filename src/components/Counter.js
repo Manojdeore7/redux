bd6edb9e43e308366eee055463838573b744a81b
Manojdeore7 <manojddeore11>
2023-06-12 11:30:27 +0530

    basic redux work

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,21 +2,28 @@ import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
   let counter = useSelector((state) => state.counter);
+  let show = useSelector((state) => state.show);
   let dispatch = useDispatch();
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
   const onIncreamentHandler = () => {
     dispatch({ type: "increment" });
   };
   const ondecreamentHandler = () => {
     dispatch({ type: "decrement" });
   };
+  const onIncreaseHandler = () => {
+    dispatch({ type: "increase", amount: 10 });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={onIncreamentHandler}>increment</button>
+        <button onClick={onIncreaseHandler}>Increase By</button>
         <button onClick={ondecreamentHandler}>decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/strore/index.js
+++ src/strore/index.js
@@ -1,13 +1,31 @@
 import { createStore } from "redux";
-let counterReducer = (state = { counter: 0 }, action) => {
+let initialState = {
+  counter: 0,
+  show: true,
+};
+let counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
+      show: state.show,
     };
   }
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      show: state.show,
+    };
+  }
+  if (action.type === "toggle") {
+    return {
+      counter: state.counter,
+      show: !state.show,
+    };
+  }
+  if (action.type === "increase") {
+    return {
+      show: state.show,
+      counter: state.counter + action.amount,
     };
   }
   return state;
